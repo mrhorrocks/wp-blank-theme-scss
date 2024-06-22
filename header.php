c9e797b7f04f1cf855a4934cc4262adfabb7367a
Mark Horrocks <mr.horrocks@gmail.com>
2024-06-22 02:00:59 +0100

    header layout done

--- header.php
+++ header.php
@@ -48,44 +48,6 @@
                 </nav>
             </div>
 
-
-
-
-            <!-- <div class="grid cols-2">
-                <div class="col-span-2">
-
-                    <div id="logo">
-                        <?php if (get_header_image()) : ?>
-                            <h1>
-                                <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" title="<?php echo esc_attr(get_bloginfo('name')); ?>">
-                                    <img src="<?php header_image(); ?>" width="<?php echo absint(get_custom_header()->width); ?>" height="<?php echo absint(get_custom_header()->height); ?>" alt="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>">
-                                </a>
-                            </h1>
-                        <?php endif; ?>
-                    </div>
-
-                    <nav id="menu" role="navigation">
-                        <div class="desktop-links">
-                            <?php
-                            wp_nav_menu(
-                                array(
-                                    'theme_location' => 'main-menu',
-                                    'container' => 'ul',
-                                    'menu_class' => 'top-nav-menu'
-                                )
-                            );
-                            ?>
-                        </div>
-                        <div class="hamburger">
-                            <button>
-                                <?php include 'hamburger_icon.php'; ?>
-                            </button>
-                        </div>
-                    </nav>
-
-                </div>
-            </div> -->
-
             <!-- APP DESCRIPTION -->
             <?php if (get_bloginfo('description')  !== '') { ?>
                 <div id="app-description">
@@ -99,8 +61,6 @@
                 <?php get_search_form(); ?>
             </div>
             <!-- SEARCH -->
-    </div>
 
-
-    </header>
+        </header>
     </div>
